9b4cab230db555ee1383b5b61548103b7b1ab57e
Jibon Rahman <95877747+N-Nothing@users.noreply.github.com>
2025-10-17 22:09:42 -0400

    initial commit

---
+++ damage-reduction.py
@@ -0,0 +1,19 @@
+import math
+
+howMany = input("How many DR are you stacking?\n")
+print("what are they?")
+
+DR = []
+for i in range(int(howMany)):
+    value = float(input())  # convert input to number
+    DR.append(value)
+
+result = [100 - dr for dr in DR]
+converted = [dr / 100 for dr in result]
+
+total = math.prod(converted)
+
+print(result)
+print(converted)
+print(100 - total)
+
